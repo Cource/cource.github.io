1422350cd375ad3d349ffb93b4a3210ac03b9ff9
cource <jeffjacobjoy@gmail.com>
2024-02-02 12:26:39 +0530

    feat: added Blog, and did some code quality fixes

---
+++ assets/blogs/test.md
@@ -0,0 +1,24 @@
+# Emacs is a cool tool
+Once you go emacs, there aint no going back. This is nice. These are
+sentences and some phrases which may be statements or questions.
+Either way,it does not matter, as one would be able to parse it as
+such anyway.
+
+## The usecase
+You can use emacs for various purposes like:
+- Writing
+- Editing Code
+So, basically just text manipulation. But there are folks who have
+added functionality like using emacs as an interface for an LLM, or
+using it as a desktop window manager itself (EXWM is such a project).
+
+## Configuration
+One of the best things about emacs is that you can configure it in an
+actual programming language, that being LISP. Emacs uses a specific
+flavour of LISP called elisp or emacs-lisp.
+Here is a byte of elisp code:
+```lisp
+(defun real-code (a, b)
+       (+ a b))
+(real-code 1 2)
+```
--- src/Main.elm
+++ src/Main.elm
@@ -1,57 +1,135 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, a, div, h1, img, text)
+import Browser.Navigation as Nav
+import Html exposing (Html, a, div, h1, img, text, p)
 import Html.Attributes exposing (alt, class, href, src, style)
+import Http
+import Markdown
+import Url
+import Url.Parser exposing (Parser, parse, map, oneOf, s, top, string, (</>))
 
+type Page
+    = Home
+    | Blog String
+    | PageNotFound
 
-type alias Image =
-    { path : String
-    , alt : String
+pageFromUrl : Url.Url -> Page
+pageFromUrl url =
+    case parse pageParser url of
+        Nothing -> PageNotFound
+        Just page -> page
+
+pageParser : Parser (Page -> a) a
+pageParser =
+    oneOf [ map Home top
+          , map Blog (s "blog" </> string)
+          ]
+
+type FailableLoad a
+    = Failure
+    | Loading
+    | Success a
+
+type alias Model =
+    { key : Nav.Key
+    , page : Page
+    , blogContent : FailableLoad String
     }
 
+type Msg
+    = LinkClicked Browser.UrlRequest
+    | UrlChanged Url.Url
+    | GotBlogContent (Result Http.Error String)
 
-type alias Project =
-    { name : String
-    , description : String
-    , image : Image
-    , techs : List Image
-    , link : String
-    }
+main : Program () Model Msg
+main = Browser.application
+       { init = init
+       , view = view
+       , update = update
+       , subscriptions = (\_ -> Sub.none)
+       , onUrlRequest = LinkClicked
+       , onUrlChange = UrlChanged
+       }
+
+init : () -> Url.Url -> Nav.Key -> (Model, Cmd Msg)
+init _ url key =
+    let pa = pageFromUrl url
+    in (Model key pa Loading, blogCmd pa)
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+    case msg of
+        LinkClicked urlReq ->
+            case urlReq of
+                Browser.Internal url ->
+n                    (model, Nav.pushUrl model.key (Url.toString url))
+                Browser.External href ->
+                    (model, Nav.load href)
+        UrlChanged url ->
+            let pa = pageFromUrl url
+            in ({model|page = pa}, blogCmd pa)
+        GotBlogContent content ->
+            ({model|blogContent =
+                  case content of
+                      Err _ -> Failure
+                      Ok val -> Success val
+             }, Cmd.none)
+
+blogCmd : Page -> Cmd Msg
+blogCmd pa =
+    case pa of
+        Blog blogId -> getBlog blogId
+        _ -> Cmd.none
+
+getBlog : String -> Cmd Msg
+getBlog blogId =
+    Http.get
+        { url = "/assets/blogs/" ++ blogId ++ ".md"
+        , expect  = Http.expectString GotBlogContent
+        }
 
 
-testProjects : List Project
-testProjects =
-    [ { name = "Shopfront"
-      , description = "A simplified shop management system for supermarkets"
-      , image = { path = "assets/images/shopfront.png", alt = "shopfront" }
-      , techs = [ { path = "assets/svgs/figma.svg", alt = "figma" }, { path = "assets/svgs/react.svg", alt = "reactjs" } ]
-      , link = "#"
-      }
-    , { name = "VoteCamp"
-      , description = "Political data collection utility for election campaigning."
-      , image = { path = "assets/images/voteCamp.png", alt = "votecamp" }
-      , techs = [ { path = "assets/svgs/figma.svg", alt = "figma" }, { path = "assets/svgs/react.svg", alt = "reactjs" }, { path = "assets/svgs/typescript.svg", alt = "typescript" } ]
-      , link = "#"
-      }
-    ]
 
 
-main =
-    div [ class "app" ]
-        [ logo
-        , div [ class "home" ]
-            [ hero
-            , portfolio testProjects
-            ]
-        , footer
+view : Model -> Browser.Document Msg
+view model =
+    { title = "Jeff's blog"
+    , body =
+        [ div [ class "app" ]
+              [ logo
+              , case model.page of
+                  Home ->
+                      div [ class "home" ]
+                          [ hero
+                          , portfolio testProjects
+                          ]
+                  Blog _ -> viewBlog model.blogContent
+                  PageNotFound -> page404
+              , footer
+              ]
         ]
+    }
 
+page404 : Html msg
+page404 = div [ class "404" ]
+          [ h1 [] [text "404"]
+          , p [] [text "Page Not Found"]
+          ]
 
+viewBlog : FailableLoad String -> Html msg
+viewBlog status =
+    case status of
+        Failure -> page404
+        Loading -> text "Loading Content..."
+        Success content ->
+            Markdown.toHtml [class "blog-content"] content
+
+logo : Html msg
 logo =
     div [ class "logo" ] [ img [ src "assets/svgs/J.svg", alt "logo" ] [] ]
 
-
+hero : Html msg
 hero =
     div [ class "hero" ]
         [ div [ class "center-text" ]
@@ -69,7 +147,7 @@ hero =
         ]
 
 
-portfolio : List Project -> Html msg
+portfolio : List ProjectInfo -> Html msg
 portfolio projects =
     div [ class "projects" ]
         [ h1 [] [ text "Projects" ]
@@ -78,7 +156,42 @@ portfolio projects =
         ]
 
 
-projectCard : Project -> Html msg
+type alias Image =
+    { path : String
+    , alt : String
+    }
+
+type alias ProjectInfo =
+    { name : String
+    , description : String
+    , image : Image
+    , techs : List Image
+    , link : String
+    }
+
+techImages =
+    { figma = { path = "assets/svgs/figma.svg", alt = "figma" }
+    , reactjs = { path = "assets/svgs/react.svg", alt = "reactjs" }
+    , typescript = { path = "assets/svgs/typescript.svg", alt = "typescript" }
+    }
+
+testProjects : List ProjectInfo
+testProjects =
+    [ { name = "Shopfront"
+      , description = "A simplified shop management system for supermarkets"
+      , image = { path = "assets/images/shopfront.png", alt = "shopfront" }
+      , techs = [ techImages.figma, techImages.reactjs ]
+      , link = "#"
+      }
+    , { name = "VoteCamp"
+      , description = "Political data collection utility for election campaigning."
+      , image = { path = "assets/images/voteCamp.png", alt = "votecamp" }
+      , techs = [ techImages.figma, techImages.reactjs, techImages.typescript ]
+      , link = "#"
+      }
+    ]
+
+projectCard : ProjectInfo -> Html msg
 projectCard project =
     a [ class "project-card", href project.link ]
         [ div [ class "project-details" ]
@@ -101,7 +214,7 @@ projectCard project =
             []
         ]
 
-
+footer : Html msg
 footer =
     div [ class "footer" ]
         [ div [ class "nopyright" ]
